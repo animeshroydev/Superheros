b4bd75ce5d5a8fc5be45806095423f33f3b315d0
animesh <royanimesh2211@gmail.com>
2024-09-03 21:49:16 +0530

    Initial commit

---
+++ app/src/main/java/com/appmakerszone/superheros/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.appmakerszone.superheros
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.appmakerszone.superheros.ui.theme.SuperherosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperherosTheme {
+                    Greeting()
+                }
+            }
+        }
+    }
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello Animesh!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SuperherosTheme {
+        Greeting()
+    }
+}

ed33d7b52962f24f96d03eab969664aa2a31f268
animesh <royanimesh2211@gmail.com>
2024-09-04 23:27:02 +0530

    added Hero model data class

---
+++ app/src/main/java/com/appmakerszone/superheros/model/Hero.kt
@@ -0,0 +1,16 @@
+package com.appmakerszone.superheros.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+
+    @StringRes
+    val nameRes: Int,
+
+    @StringRes
+    val descriptionRes: Int,
+
+    @DrawableRes
+    val imageRes: Int
+)

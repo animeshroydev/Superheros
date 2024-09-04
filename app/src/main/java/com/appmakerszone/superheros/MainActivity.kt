028013b500389cd6c9d46bf9a6663e73b2ccd708
animesh <royanimesh2211@gmail.com>
2024-09-05 00:18:39 +0530

    list item in action

--- app/src/main/java/com/appmakerszone/superheros/MainActivity.kt
+++ app/src/main/java/com/appmakerszone/superheros/MainActivity.kt
@@ -3,10 +3,23 @@ package com.appmakerszone.superheros
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.appmakerszone.superheros.model.Hero
+import com.appmakerszone.superheros.model.HeroesRepository
 import com.appmakerszone.superheros.ui.theme.SuperherosTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,11 +27,76 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SuperherosTheme {
-                    Greeting()
-                }
+
+          HeroesApp()
+
             }
         }
     }
+}
+
+@Composable
+fun HeroesApp() {
+
+ LazyColumn {
+     items(HeroesRepository.heroes) {
+         HeroItems(it)
+     }
+ }
+
+}
+
+@Composable
+fun HeroItems(hero: Hero, modifier: Modifier = Modifier) {
+
+    Card(modifier = modifier) {
+
+        Row {
+
+            HeroesInformation(
+                title = hero.nameRes,
+                description = hero.descriptionRes
+            )
+
+            HeroesImage(heroImage = hero.imageRes)
+
+        }
+
+    }
+
+}
+
+@Composable
+fun HeroesInformation(
+    @StringRes title: Int,
+    @StringRes description: Int, modifier: Modifier = Modifier
+) {
+
+    Column(modifier = modifier) {
+
+        Text(
+            text = stringResource(id = title),
+            style = MaterialTheme.typography.labelSmall
+        )
+
+        Text(
+            text = stringResource(id = description),
+            style = MaterialTheme.typography.labelSmall
+        )
+
+    }
+
+}
+
+@Composable
+fun HeroesImage(@DrawableRes heroImage: Int, modifier: Modifier = Modifier) {
+
+    Image(
+        painter = painterResource(id = heroImage),
+        contentDescription = null
+    )
+
+}
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
@@ -32,6 +110,6 @@ fun Greeting(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SuperherosTheme {
-        Greeting()
+        HeroesApp()
     }
 }

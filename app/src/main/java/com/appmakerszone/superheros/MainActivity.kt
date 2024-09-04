858c572a0378b7fd57c5c8963dfec6a95ff942c8
animesh <royanimesh2211@gmail.com>
2024-09-05 01:34:14 +0530

    UI polish done

--- app/src/main/java/com/appmakerszone/superheros/MainActivity.kt
+++ app/src/main/java/com/appmakerszone/superheros/MainActivity.kt
@@ -6,20 +6,32 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.appmakerszone.superheros.model.Hero
 import com.appmakerszone.superheros.model.HeroesRepository
+import com.appmakerszone.superheros.ui.theme.Shapes
 import com.appmakerszone.superheros.ui.theme.SuperherosTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +40,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperherosTheme {
 
-          HeroesApp()
+                HeroesApp()
 
             }
         }
@@ -38,30 +50,40 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun HeroesApp() {
 
- LazyColumn {
-     items(HeroesRepository.heroes) {
-         HeroItems(it)
-     }
- }
+    LazyColumn {
+        items(HeroesRepository.heroes) {
+            HeroItems(it)
+        }
+    }
 
 }
 
 @Composable
 fun HeroItems(hero: Hero, modifier: Modifier = Modifier) {
 
-    Card(modifier = modifier) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+    ) {
 
-        Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
-            HeroesInformation(
-                title = hero.nameRes,
-                description = hero.descriptionRes
-            )
+                HeroesInformation(
+                    title = hero.nameRes,
+                    description = hero.descriptionRes
+                )
 
-            HeroesImage(heroImage = hero.imageRes)
+
+                HeroesImage(heroImage = hero.imageRes)
+
 
         }
-
     }
 
 }
@@ -72,16 +94,20 @@ fun HeroesInformation(
     @StringRes description: Int, modifier: Modifier = Modifier
 ) {
 
-    Column(modifier = modifier) {
+
+    Column(modifier = modifier.padding(20.dp)) {
 
         Text(
             text = stringResource(id = title),
-            style = MaterialTheme.typography.labelSmall
+            style = MaterialTheme.typography.displaySmall
         )
 
         Text(
+            modifier = Modifier.width(200.dp),
             text = stringResource(id = description),
-            style = MaterialTheme.typography.labelSmall
+            maxLines = 4,
+            overflow = TextOverflow.Ellipsis,
+            style = MaterialTheme.typography.bodyLarge,
         )
 
     }
@@ -91,19 +117,15 @@ fun HeroesInformation(
 @Composable
 fun HeroesImage(@DrawableRes heroImage: Int, modifier: Modifier = Modifier) {
 
-    Image(
-        painter = painterResource(id = heroImage),
-        contentDescription = null
-    )
+    Box(modifier = Modifier.size(100.dp).padding(10.dp).padding(end = 6.dp)) {
+        Image(
+            painter = painterResource(id = heroImage),
+            contentDescription = null,
+            modifier = Modifier.clip(Shapes.small),
+            contentScale = ContentScale.Crop,
 
-}
-
-@Composable
-fun Greeting(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Animesh!",
-        modifier = modifier
-    )
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
